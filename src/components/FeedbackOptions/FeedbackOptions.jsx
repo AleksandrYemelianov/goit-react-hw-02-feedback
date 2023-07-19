3fb5d3511458fb39dd7f5fe366672815db0cb584
Aleksandr Yemelianov <emelyanon@gmail.com>
2023-07-19 22:04:59 +0200

    add nanoid to options btn

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css';
+import { nanoid } from 'nanoid';
 
 
 const FeedbackOptions = ({ options, handleIncrement }) => {
     return (
         <div className={css.btnGroup}>
-            {options.map((option, i) => {
+            {options.map((option) => {
                 return (
-                    <button type='button' key={[i]} className={css.btn} onClick={() => handleIncrement(option)}>
+                    <button type='button' key={nanoid()} className={css.btn} onClick={() => handleIncrement(option)}>
                         {option}
                     </button>
                 )
